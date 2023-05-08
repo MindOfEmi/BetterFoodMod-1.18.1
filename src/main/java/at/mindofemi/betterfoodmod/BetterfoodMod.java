d828322aaf1b2dc706d174cfa43f5aacef532eaf
MindOfEmi <emilianecker@gmail.com>
2023-05-08 13:49:56 +0200

    Added a Item

--- src/main/java/at/mindofemi/betterfoodmod/BetterfoodMod.java
+++ src/main/java/at/mindofemi/betterfoodmod/BetterfoodMod.java
@@ -1,9 +1,11 @@
 package at.mindofemi.betterfoodmod;
 
+import at.mindofemi.betterfoodmod.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -26,8 +28,13 @@ public class BetterfoodMod {
     private static final Logger LOGGER = LogManager.getLogger();
 
     public BetterfoodMod() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
+
+
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
