5f2504906fa757268cd431cda3a309112a506413
MindOfEmi <emilianecker@gmail.com>
2023-05-19 12:38:49 +0200

    Added a few Food Items

--- src/main/java/at/mindofemi/betterfoodmod/item/ModFoods.java
+++ src/main/java/at/mindofemi/betterfoodmod/item/ModFoods.java
@@ -6,6 +6,14 @@ import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
     public static final FoodProperties BANANA = (new FoodProperties.Builder()).nutrition(4).saturationMod(0.5F).build();
-    public static final FoodProperties GREEN_LEAVES = (new FoodProperties.Builder()).alwaysEat().nutrition(20).saturationMod(20f).effect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 200, 1000), 1.0F).effect(new MobEffectInstance(MobEffects.REGENERATION, 200, 1000), 1.0F).effect(new MobEffectInstance(MobEffects.HEALTH_BOOST, 200, 1000), 1.0F).effect(new MobEffectInstance(MobEffects.HEAL, 200, 1000), 1.0F).effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200, 4), 1.0F).build();
+    public static final FoodProperties GREEN_LEAVES = (new FoodProperties.Builder()).alwaysEat().nutrition(20).saturationMod(20f).effect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 200, 1000), 1.0F).effect(new MobEffectInstance(MobEffects.REGENERATION, 200, 1000), 1.0F).effect(new MobEffectInstance(MobEffects.HEALTH_BOOST, 200, 1000), 1.0F).effect(new MobEffectInstance(MobEffects.HEAL, 200, 1000), 1.0F).effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200, 3), 1.0F).effect(new MobEffectInstance(MobEffects.HUNGER, 200, 2), 1.0F).build();
+    public static final FoodProperties POMMES = (new FoodProperties.Builder()).nutrition(4).saturationMod(0.2F).build();
+    public static final FoodProperties PATTY = (new FoodProperties.Builder()).nutrition(1).saturationMod(0.2F).meat().build();
+    public static final FoodProperties HAM = (new FoodProperties.Builder()).nutrition(1).saturationMod(0.1F).meat().build();
+    public static final FoodProperties HAMBURGER = (new FoodProperties.Builder()).nutrition(12).saturationMod(0.6F).meat().build();
+    public static final FoodProperties TOMATO = (new FoodProperties.Builder()).nutrition(3).saturationMod(0.5F).build();
+    public static final FoodProperties PIZZA = (new FoodProperties.Builder()).nutrition(10).saturationMod(0.8F).build();
+    public static final FoodProperties TORTILLIA = (new FoodProperties.Builder()).nutrition(4).saturationMod(0.5F).build();
+    public static final FoodProperties WRAP = (new FoodProperties.Builder()).nutrition(7).saturationMod(0.5F).build();
 
 }
--- src/main/java/at/mindofemi/betterfoodmod/item/ModItems.java
+++ src/main/java/at/mindofemi/betterfoodmod/item/ModItems.java
@@ -14,11 +14,38 @@ public class ModItems {
     public static final RegistryObject<Item> CITRINE = ITEMS.register("citrine",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERFOOD_TAB)));
 
+    public static final RegistryObject<Item> FLOUR = ITEMS.register("flour",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERFOOD_TAB)));
+
     public static final RegistryObject<Item> BANANA = ITEMS.register("banana",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERFOOD_TAB).food(ModFoods.BANANA)));
 
     public static final RegistryObject<Item> GREEN_LEAVES = ITEMS.register("green_leaves",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERFOOD_TAB).food(ModFoods.GREEN_LEAVES)));
+
+    public static final RegistryObject<Item> POMMES = ITEMS.register("pommes",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERFOOD_TAB).food(ModFoods.POMMES)));
+
+    public static final RegistryObject<Item> PATTY = ITEMS.register("patty",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERFOOD_TAB).food(ModFoods.PATTY)));
+
+    public static final RegistryObject<Item> HAM = ITEMS.register("ham",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERFOOD_TAB).food(ModFoods.HAM)));
+
+    public static final RegistryObject<Item> HAMBURGER = ITEMS.register("hamburger",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERFOOD_TAB).food(ModFoods.HAMBURGER)));
+
+    public static final RegistryObject<Item> TOMATO = ITEMS.register("tomato",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERFOOD_TAB).food(ModFoods.TOMATO)));
+
+    public static final RegistryObject<Item> PIZZA = ITEMS.register("pizza",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERFOOD_TAB).food(ModFoods.PIZZA)));
+
+    public static final RegistryObject<Item> TORTILLIA = ITEMS.register("tortillia",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERFOOD_TAB).food(ModFoods.TORTILLIA)));
+
+    public static final RegistryObject<Item> WRAP = ITEMS.register("wrap",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERFOOD_TAB).food(ModFoods.WRAP)));
 
 
 
